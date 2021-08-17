4bafa46469f10b87c63629ea10f71b5fa40a2ae0
marieLataste <serialmother4@gmail.com>
2021-08-17 10:59:06 +0200

    Falling down
    JS => createElement
    css => raining

---
+++ main.js
@@ -0,0 +1,15 @@
+function createFalls(){
+
+    let container = document.querySelector('.container');
+    let span = document.createElement('span');
+
+    span.style.left = Math.random() * innerWidth + 'px';
+
+    container.appendChild(span);
+
+    setTimeout(() => {
+        span.remove();
+    }, 5000);
+
+}
+setInterval(createFalls, 100);
