ae405555039c2046788915f42b13289cfd08bd1c
marieLataste <serialmother4@gmail.com>
2021-08-17 11:22:52 +0200

    countdown
    start the countdown til' helloween

--- main.js
+++ main.js
@@ -13,3 +13,29 @@ function createFalls(){
 
 }
 setInterval(createFalls, 100);
+
+let countDate = new Date('oct 31, 2021 00:01').getTime();
+
+function countDown(){
+    let now = new Date().getTime();
+    gap = countDate - now;
+
+    let seconds = 1000;
+    let minutes = seconds * 60;
+    let hours = minutes * 60;
+    let days = hours * 24;
+
+    let d = Math.floor(gap / (days));
+    let h = Math.floor((gap % (days)) / (hours));
+    let m = Math.floor((gap % (hours)) / (minutes));
+    let s = Math.floor((gap % (minutes)) / (seconds));
+
+    document.getElementById('days').innerText = d;
+    document.getElementById('hours').innerText = h;
+    document.getElementById('minutes').innerText = m;
+    document.getElementById('seconds').innerText = s;
+
+}
+setInterval(function() {
+    countDown();
+}, 1000);
